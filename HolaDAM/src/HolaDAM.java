0ca9011a94a05486c0e5e3fd6d64f655cebfe81b
AdrianGarciaAndreu <adrielakseir@gmail.com>
2014-04-29 16:33:22 +0200

    Añadido el sistem out println
    Signed-off-by: AdrianGarciaAndreu <adrielakseir@gmail.com>

--- HolaDAM/src/HolaDAM.java
+++ HolaDAM/src/HolaDAM.java
@@ -1,8 +1,16 @@
+/**
+ * 
+ * @author alejandro
+ *
+ */
+
 public class HolaDAM {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//jejeje
+		
+		System.out.println("jo jo joooooo By Adrian");
 
 	}
 
